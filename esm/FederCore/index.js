8d5b39c9cd731a785f23d1b7a98f16b0c3a11da7
min.tian <min.tian.cn@gmail.com>
2022-01-28 15:04:58 +0800

    [feature] Add faiss-ivf-flat search function, indexMeta, projector (t-sne and umap)
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

--- esm/FederCore/index.js
+++ esm/FederCore/index.js
@@ -1,15 +1,37 @@
-import { project } from '../Utils/index.js';
+import Projector from '../Utils/projector/index.js';
 import faissIndexParser from './faissIndexParser.js';
+import faissIVFFlatSearch from './faissIVFFlatSearch.js';
+import { IndexType } from '../Utils/config.js';
+import { ProjectMethod } from '../Utils/projector/index.js';
 
 /* Feder core class */
 export default class FederCore {
-  constructor({ data, source = 'faiss', projectParams = {} }) {
+  constructor({
+    data,
+    source = 'faiss',
+    projectMethod = ProjectMethod.UMAP,
+    projectParams = {},
+  }) {
+    this.index = null;
+    this.meta = null;
+    this.indexParser = null;
+    this.indexSearchHandler = null;
+    this.projector = null;
+
     this.data = data;
     this.setIndexSource(source);
     this.parseIndex();
+    this.setIndexSearchHandler();
+    this.updateId2Vec();
+
+    this.initProjector(projectMethod, projectParams);
+
+    // need parsed_index and projector.
+    this.updateIndexMeta();
   }
   setIndexSource(source) {
     this.indexParser = null;
+    this.indexSource = source;
     if (source === 'faiss') {
       this.indexParser = faissIndexParser;
     }
@@ -17,31 +39,108 @@ export default class FederCore {
   parseIndex() {
     if (this.indexParser) {
       this.index = this.indexParser(this.data);
-      console.log('index', this.index);
     } else {
-      console.error('no parser found');
+      console.error('No parser found');
+    }
+  }
+  setIndexSearchHandler() {
+    this.indexSearchHandler = null;
+    if (this.index) {
+      if (this.indexSource === 'faiss') {
+        if (this.index.indexType === IndexType.IVFFlat) {
+          this.indexSearchHandler = faissIVFFlatSearch;
+        }
+        if (this.index.indexType === IndexType.HNSW) {
+          // todo - search for faiss-hnsw
+        }
+      } else if (this.source === 'hnswlib') {
+        if (this.index.indexType === IndexType.HNSW) {
+          // todo - search for hnswlib-hnsw
+        }
+      } else {
+        console.error('Unknown index source');
+      }
+    } else {
+      console.error('Unknown index');
     }
   }
 
-  get meta() {
-    // define ivf-meta
-    // define hnsw-meta
+  updateId2Vec() {
+    const id2vector = {};
+    if (this.index) {
+      if (this.index.indexType === IndexType.IVFFlat) {
+        const inv = this.index.invlists;
+        for (let list_no = 0; list_no < inv.nlist; list_no++) {
+          inv.data[list_no].ids.forEach((id, ofs) => {
+            id2vector[id] = inv.data[list_no].vectors[ofs];
+          });
+        }
+      }
+      if (this.index.indexType === IndexType.HNSW) {
+        // todo
+      }
+    }
+    this.id2vector = id2vector;
   }
-  get id2Vector() {
-    // id to vector map
+  updateIndexMeta() {
+    const indexMeta = {};
+    if (this.index) {
+      if (this.index.indexType === IndexType.IVFFlat) {
+        indexMeta.ntotal = this.index.ntotal;
+        indexMeta.nlist = this.index.nlist;
+        // indexMeta.listCentroidVectors = this.index.childIndex.vectors;
+        indexMeta.listCentroidProjections = this.project(
+          this.index.childIndex.vectors
+        );
+        indexMeta.listIds = this.index.invlists.data.map((d) => d.ids);
+      }
+      if (this.index.indexType === IndexType.HNSW) {
+        // todo
+      }
+    }
+    this.indexMeta = indexMeta;
   }
-  search() {
-    // define ivf-vis_records
-    // define hnsw-vis_records
+
+  getTestIdAndVec() {
+    if (!this.index) {
+      return [null, null];
+    }
+    const ids = Object.keys(this.id2vector);
+    const r = Math.floor(Math.random() * ids.length);
+    const testId = ids[r];
+    const testVec = this.id2vector[testId];
+    return { testId, testVec };
   }
-  project() {
-    project(...arguments);
+
+  getVectoreById(id) {
+    if (id in this.id2Vector) {
+      return this.id2Vector[id];
+    } else {
+      return null;
+    }
   }
-  setProjectParams() {}
-  _parseFaissIVFFlat() {
-    this.index = index;
+  setSearchParams(params) {
+    this.searchParams = Object.assign(this.searchParams, params);
   }
-  _parseHNSWlibHNSW() {
-    this.index = index;
+  search(target) {
+    const res = this.indexSearchHandler({
+      index: this.index,
+      target,
+      params: this.searchParams,
+    });
+    return res;
+  }
+
+  initProjector(projectMethod, projectParams = {}) {
+    this.projector = new Projector(projectMethod, projectParams);
+  }
+  setProjectParams(projectMethod, projectParams = {}) {
+    if (this.projector) {
+      this.projector.setProjectParams(projectMethod, projectParams);
+      this.updateIndexMeta();
+    }
+  }
+  project(vectors) {
+    return this.projector.project(vectors);
   }
 }
---
+++ esm/Utils/config.js
@@ -0,0 +1,7 @@
+export const IndexType = {
+  IVFFlat: 'IVFFlat',
+  FlatL2: 'FlatL2',
+  FlatIR: 'FlatIR',
+  Flat: 'Flat',
+  HNSW: 'HNSW',
+}
