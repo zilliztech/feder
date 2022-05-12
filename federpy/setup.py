bb2fe75c4029cba37963d6991774c9cca1b854d5
min.tian <min.tian.cn@gmail.com>
2022-05-12 09:36:14 +0800

    ..
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

---
+++ federpy/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="federpy",
+    version="0.3.0",
+    author="min.tian@zilliz",
+    author_email="min.tian@zilliz.com",
+    description="Feder for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zilliztech/feder",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ federpy/src/federpy/federpy.py
@@ -0,0 +1,92 @@
+from IPython.display import display, HTML
+import random
+
+class FederPy:
+    def __init__(self, indexFile, indexSource, width=1000, height=620, mediaType="", mediaUrls=[]):
+        self.indexFile = indexFile
+        self.indexSource = indexSource
+
+        self.container = "feder-container-%s" % random.randint(0, 10000000)
+
+        self.federjs = "https://unpkg.com/@zilliz/feder"
+        # self.federjs = "http://192.168.0.101:12357/feder_esm.js"
+
+        self.actionJs = ""
+        self.searchParams = {}
+        self.width = width
+        self.height = height
+        self.mediaType = mediaType
+        self.mediaUrls = mediaUrls
+
+    def getDiv(self):
+        return '<div id="%s" />' % self.container
+
+    def getInitJs(self):
+        return """
+import { Feder } from "%s"
+// console.log(Feder)
+
+const mediaUrls = [%s]
+const mediaCallback = (rowId) => rowId in mediaUrls ? mediaUrls[rowId] : null
+
+const feder = new Feder({
+    filePath: "%s",
+    source: "%s",
+    domSelector: "#%s",
+    viewParams: {
+        width: "%s",
+        height: "%s",
+        mediaType: "%s",
+        mediaCallback,
+    }
+})
+        """ % (self.federjs, ",".join(["'%s'" % url for url in self.mediaUrls]), self.indexFile, self.indexSource, self.container, self.width, self.height, self.mediaType)
+
+    def overview(self, isDisplay=True):
+        self.actionJs = "feder.overview()"
+        if isDisplay:
+            self.showHtml()
+        else:
+            return self.getHtml()
+
+    def searchRandTestVec(self, isDisplay=True):
+        self.actionJs = "feder.setSearchParams(%s)\nfeder.searchRandTestVec()" % self.searchParams
+        if isDisplay:
+            self.showHtml()
+        else:
+            return self.getHtml()
+
+    def setSearchParams(self, searchParams):
+        self.searchParams = searchParams
+
+    def getJs(self):
+        return """
+%s
+%s
+        """ % (self.getInitJs(), self.actionJs)
+
+    def getHtml(self):
+        return """
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="theme-color" content="#ffffff" />
+  <title>Feder</title>
+</head>
+
+<body style="background: #000">
+  %s
+</body>
+
+<script type="module">
+%s
+</script>
+
+</html> 
+""" % (self.getDiv(), self.getJs())
+
+    def showHtml(self):
+        display(HTML(self.getHtml()))
