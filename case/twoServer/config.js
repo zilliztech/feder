30b06dcc7715e690e13682b36b83409e0ef1aaf2
min.tian <min.tian.cn@gmail.com>
2022-09-29 11:15:40 +0800

    ..
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

--- case/twoServer/config.js
+++ case/twoServer/config.js
@@ -3,9 +3,6 @@ export const federIndexUrl = `http://localhost:${federIndexServerPort}`;
 export const federLayoutServerPort = 12358;
 export const federLayoutUrl = `http://localhost:${federLayoutServerPort}`;
 
-export const federIndexLayoutServerPort = 12359;
-export const federIndexLayoutUrl = `http://localhost:${federIndexLayoutServerPort}`;
-
 export const federIndexFuncs = {
   getIndexType: 'getIndexType',
   getIndexMeta: 'getIndexMeta',
